341537494def776e131348e4d333bab513f55cbc
likithkandepu13 <withlikith@gmail.com>
2024-12-07 08:14:29 +0530

    changes

---
+++ src/main/java/com/klef/jfsd/exam/JfsdLabEndsemlab1Application.java
@@ -0,0 +1,14 @@
+package com.klef.jfsd.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JfsdLabEndsemlab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JfsdLabEndsemlab1Application.class, args);
+		System.out.println("System is loading11");
+	}
+
+}
---
+++ src/main/java/com/klef/jfsd/exam/model/Product.java
@@ -0,0 +1,59 @@
+package com.klef.jfsd.exam.model;
+
+public class Product {
+    private int id;
+    private String title;
+    private String price;
+    private String description;
+    private String image;
+    private String category;
+
+    // Getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
